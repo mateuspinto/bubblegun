9b5b17b1296243b8312ea0b885ca7bd8f41073f5
Mateus Pinto da Silva <mateus.p.silva@ufv.br>
2021-12-18 17:40:59 -0300

    added xml, csv, json download

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
-from flask import Flask, g, request
+from flask import Flask, g, request, make_response
 import sqlite3
 from flask_cors import CORS
 from datetime import date
 import re
+from io import StringIO
+import csv
+from dicttoxml import dicttoxml
+import json
 
 STOPWORDS = ['de', 'a', 'o', 'que', 'e', 'do', 'da', 'em', 'um', 'para', 'é', 'com', 'não', 'uma', 'os', 'no', 'se', 'na', 'por', 'mais', 'as', 'dos', 'como', 'mas', 'foi', 'ao', 'ele', 'das', 'tem', 'à', 'seu', 'sua', 'ou', 'ser', 'quando', 'muito', 'há', 'nos', 'já', 'está', 'eu', 'também', 'só', 'pelo', 'pela', 'até', 'isso', 'ela', 'entre', 'era', 'depois', 'sem', 'mesmo', 'aos', 'ter', 'seus', 'quem', 'nas', 'me', 'esse', 'eles', 'estão', 'você', 'tinha', 'foram', 'essa', 'num', 'nem', 'suas', 'meu', 'às', 'minha', 'têm', 'numa', 'pelos', 'elas', 'havia', 'seja', 'qual', 'será', 'nós', 'tenho', 'lhe', 'deles', 'essas', 'esses', 'pelas', 'este', 'fosse', 'dele', 'tu', 'te', 'vocês', 'vos', 'lhes', 'meus', 'minhas', 'teu', 'tua', 'teus', 'tuas', 'nosso', 'nossa', 'nossos', 'nossas', 'dela', 'delas', 'esta', 'estes', 'estas', 'aquele', 'aquela', 'aqueles', 'aquelas', 'isto', 'aquilo', 'estou', 'está', 'estamos', 'estão', 'estive', 'esteve', 'estivemos', 'estiveram', 'estava', 'estávamos', 'estavam', 'estivera', 'estivéramos', 'esteja', 'estejamos', 'estejam', 'estivesse', 'estivéssemos', 'estivessem', 'estiver', 'estivermos', 'estiverem', 'hei', 'há', 'havemos', 'hão', 'houve', 'houvemos', 'houveram', 'houvera', 'houvéramos', 'haja', 'hajamos', 'hajam', 'houvesse', 'houvéssemos', 'houvessem', 'houver', 'houvermos', 'houverem', 'houverei', 'houverá', 'houveremos', 'houverão', 'houveria', 'houveríamos', 'houveriam', 'sou', 'somos', 'são', 'era', 'éramos', 'eram', 'fui', 'foi', 'fomos', 'foram', 'fora', 'fôramos', 'seja', 'sejamos', 'sejam', 'fosse', 'fôssemos', 'fossem', 'for', 'formos', 'forem', 'serei', 'será', 'seremos', 'serão', 'seria', 'seríamos', 'seriam', 'tenho', 'tem', 'temos', 'tém', 'tinha', 'tínhamos', 'tinham', 'tive', 'teve', 'tivemos', 'tiveram', 'tivera', 'tivéramos', 'tenha', 'tenhamos', 'tenham', 'tivesse', 'tivéssemos', 'tivessem', 'tiver', 'tivermos', 'tiverem', 'terei', 'terá', 'teremos', 'terão', 'teria', 'teríamos', 'teriam']
 DATABASE_FILENAME = 'database.db'
@@ -97,6 +101,82 @@ def positive_feeling_percentage():
 
     percentage /= LENGHT
     return {'error': 0, 'response': {'percentage': percentage * 100}}
+
+
+@APP.route('/csv/all_pages', methods=['GET'])
+def csv__all_pages():
+    DB_CUR = get_database().cursor()
+    dest = StringIO()
+    writer = csv.writer(dest)
+    writer.writerow(['userid', 'username', 'followers'])
+
+    for i in list(DB_CUR.execute('SELECT userid, username, followers FROM pages')):
+        writer.writerow(i)
+
+    output = make_response(dest.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=bubblegun_all_pages.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
+
+
+@APP.route('/csv/all_tweets', methods=['GET'])
+def csv__all_tweets():
+    DB_CUR = get_database().cursor()
+    dest = StringIO()
+    writer = csv.writer(dest)
+    writer.writerow(['text', 'date_day', 'date_month', 'date_year', 'positive_feeling', 'userid', 'username', 'followers'])
+
+    for i in list(DB_CUR.execute('SELECT text, date_day, date_month, date_year, positive_feeling, tweets.userid, username, followers FROM tweets INNER JOIN pages ON tweets.userid = pages.userid')):
+        writer.writerow(i)
+
+    output = make_response(dest.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=bubblegun_all_tweets.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
+
+
+@APP.route('/xml/all_pages', methods=['GET'])
+def xml__all_pages():
+    DB_CUR = get_database().cursor()
+    raw_xml = dicttoxml([{'userid': x[0], 'username': x[1], 'followers': x[2]} for x in list(DB_CUR.execute('SELECT userid, username, followers FROM pages'))], custom_root='pages', attr_type=False)
+
+    output = make_response(raw_xml)
+    output.headers["Content-Disposition"] = "attachment; filename=bubblegun_all_pages.xml"
+    output.headers["Content-type"] = "text/xml"
+    return output
+
+
+@APP.route('/xml/all_tweets', methods=['GET'])
+def xml__all_tweets():
+    DB_CUR = get_database().cursor()
+    raw_xml = dicttoxml([{'text': x[0], 'date_day':x[1], 'date_month':x[2], 'date_year':x[3], 'positive_feeling':x[4], 'userid':x[5], 'username':x[6], 'followers':x[7]} for x in list(DB_CUR.execute('SELECT text, date_day, date_month, date_year, positive_feeling, tweets.userid, username, followers FROM tweets INNER JOIN pages ON tweets.userid = pages.userid'))], custom_root='tweets', attr_type=False)
+
+    output = make_response(raw_xml)
+    output.headers["Content-Disposition"] = "attachment; filename=bubblegun_all_tweets.xml"
+    output.headers["Content-type"] = "text/xml"
+    return output
+
+
+@APP.route('/json/all_pages', methods=['GET'])
+def json__all_pages():
+    DB_CUR = get_database().cursor()
+    raw_json = json.dumps([{'userid': x[0], 'username': x[1], 'followers': x[2]} for x in list(DB_CUR.execute('SELECT userid, username, followers FROM pages'))])
+
+    output = make_response(raw_json)
+    output.headers["Content-Disposition"] = "attachment; filename=bubblegun_all_pages.json"
+    output.headers["Content-type"] = "text/json"
+    return output
+
+
+@APP.route('/json/all_tweets', methods=['GET'])
+def json__all_tweets():
+    DB_CUR = get_database().cursor()
+    raw_json = json.dumps([{'text': x[0], 'date_day':x[1], 'date_month':x[2], 'date_year':x[3], 'positive_feeling':x[4], 'userid':x[5], 'username':x[6], 'followers':x[7]} for x in list(DB_CUR.execute('SELECT text, date_day, date_month, date_year, positive_feeling, tweets.userid, username, followers FROM tweets INNER JOIN pages ON tweets.userid = pages.userid'))])
+
+    output = make_response(raw_json)
+    output.headers["Content-Disposition"] = "attachment; filename=bubblegun_all_tweets.json"
+    output.headers["Content-type"] = "text/json"
+    return output
 
 
 if __name__ == '__main__':
